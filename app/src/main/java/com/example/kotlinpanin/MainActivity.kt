3c1c4ad35f5f04e7f6333849708d7710788dbb33
Nick Panin <panin@adwz.ru>
2020-05-01 18:31:56 +0300

    main activity list added

---
+++ app/src/main/java/com/example/kotlinpanin/MainActivity.kt
@@ -0,0 +1,31 @@
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.kotlinpanin.Post
+import com.example.kotlinpanin.PostAdapter
+import com.example.kotlinpanin.R
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
+
+class MainActivity : AppCompatActivity() {
+
+    val posts: ArrayList<Post> = ArrayList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        addPosts()
+        postList.layoutManager = LinearLayoutManager(this)
+        postList.adapter = PostAdapter(posts, this)
+
+    }
+
+    fun addPosts() {
+        posts.add(Post("User One", R.mipmap.test_img_one, Date(), getString(R.string.ipsum_string), 5, 0, 0))
+        posts.add(Post("User Two", R.mipmap.test_img_two, Date(), getString(R.string.ipsum_string), 0, 10, 0))
+        posts.add(Post("User Tree", R.mipmap.test_img_three, Date(), getString(R.string.ipsum_string), 0, 0, 3))
+        posts.add(Post("User Four", R.mipmap.test_img_four, Date(), getString(R.string.ipsum_string), 3, 11, 2))
+        posts.add(Post("User Five", R.mipmap.test_img_five, Date(), getString(R.string.ipsum_string), 0, 0, 0))
+    }
+}
